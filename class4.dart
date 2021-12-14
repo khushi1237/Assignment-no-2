2be9789103932a9a691b1964a555a9d365a389de
khushi1237 <96117174+khushi1237@users.noreply.github.com>
2021-12-14 16:26:16 +0500

    Add files via upload

---
+++ class4.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+
+void main() {
+  // program 1 Q1
+  // String data = "KHUSHI";
+  // String d1 = "khushi";
+
+  // print(data.toLowerCase());
+  // print(d1.toUpperCase());
+//q2
+  // int a = 2;
+  // print((a + 5) * 10/2);
+  //q3
+
+  // var b = 3;
+  // var j = 4;
+  // var result = --b;
+  // print(result);
+
+  // var b = 3;
+  // var j = 4;
+  // var result = --b - --j;
+  // print(result);
+
+  // var b = 3;
+  // var j = 4;
+  // var result = --b - --j + ++b;
+  // print(result);
+
+  // var b = 3;
+  // var j = 4;
+  // var result = --b - --j + ++b + --b;
+  // print(result);
+
+  //q4
+  // String k1 = " khushi";
+  // String k = "nazeer";
+  // print(k1.compareTo(k));
+
+  //q5
+  // stdout.write("Enter String");
+  // String k1 = stdin.readLineSync()!;
+
+  // printk1.length);
+
+  // stdout.write("Enter capital letter");
+  // String v1 = stdin.readLineSync()!;
+  // if (v1.codeUnitAt(0) >= 65 && v1.codeUnitAt(0) <= 99) {
+  //   if (v1.length == 1) {
+  //     print("capital");
+  //   }
+  // } else {
+  //   print("Enter only one letter");
+  // }
+
+  // stdout.write("Enter small letter");
+  // String v2 = stdin.readLineSync()!;
+  // if (v2.codeUnitAt(0) >= 97 && v2.codeUnitAt(0) >= 122) {
+  //   if (v2.length == 1) {
+  //     print("small");
+  //   }
+  // } else {
+  //   print("Enter only one letter");
+  // }
+
+  // stdout.write(" Enter number");
+  // var v3 = stdin.readLineSync()!;
+  // if (v3.codeUnitAt(0) >= 49 && v3.codeUnitAt(0) <= 57) {
+  //   if (v3.length == 1) {
+  //     print("Enter only one letter");
+  //   }
+  // }
+
+//q7
+
+  // stdout.write("enter user Input:");
+  // String khushi = stdin.readLineSync()!;
+  // String a = "khushinazer";
+  // String b = a.substring(4, 7);
+  // print(b);
+
+  //q8
+  // String data = "hydrabad";
+  // var v2 = data.split('');
+  // print(v2);
+}
